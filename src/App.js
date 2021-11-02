97e0239f4757c88532ea97729927cff95be01430
md-hasan-ali <hasanaliringku@gmail.com>
2021-11-02 19:07:20 +0600

    start home page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+
 import './App.css';
+import Home from "./pages/Home/Home/Home";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Switch>
+          <Route path="/home">
+            <Home />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+
+      </Router>
     </div>
   );
 }
---
+++ src/pages/Home/Home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Navigasion from '../../Sheard/Navigasion/Navigasion';
+
+const Home = () => {
+    return (
+        <div>
+            <Navigasion></Navigasion>
+        </div>
+    );
+};
+
+export default Home;
